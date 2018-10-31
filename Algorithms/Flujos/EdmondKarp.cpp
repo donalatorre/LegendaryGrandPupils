64720e00cc8d532d32d227138c12008197aa9656
EliasMera <a01280762@itesm.mx>
2018-10-31 10:24:58 -0600

    Adds flows algorithms

---
+++ Algorithms/Flujos/EdmondKarp.cpp
@@ -0,0 +1,60 @@
+/*
+Edmonds-Karp algorithm O(VE^2)
+Max flow
+The matrix capacity stores the capacity for every pair of vertices. 
+adj is the adjacency list of the undirected graph, 
+since we have also to use the reversed of directed edges when we are looking for augmenting paths.
+
+The function maxflow will return the value of the maximal flow. 
+During the algorithm the matrix capacity will actually store the residual capacity of the network. 
+The value of the flow in each edge will actually no stored, 
+but it is easy to extent the implementation - by using an additional matrix - to also store the flow and return it.
+*/
+
+int n;
+vector<vector<int>> capacity;
+vector<vector<int>> adj;
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(n);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
