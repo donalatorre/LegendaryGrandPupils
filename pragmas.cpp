fc403e7bea9b5dc9164ec1435045c6b89fba0ba5
donalatorre <adanlopezalatorre@gmail.com>
2020-09-28 20:41:50 -0500

    added pragmas

---
+++ pragmas.cpp
@@ -0,0 +1,3 @@
+#pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline") //Optimization flags 
+#pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX 
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
